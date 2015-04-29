50fd009090592fc03517d7fdd6e5861775594115
ubu2 <ubu2@m-ubu2>
2015-04-29 20:11:30 +0200

    Versio 0.2 del projecte prjava03Mateo. Branca00 creada i modificada

--- src/prjava03mateo/Prjava03Mateo.java
+++ src/prjava03mateo/Prjava03Mateo.java
@@ -32,8 +32,8 @@ public class Prjava03Mateo extends Frame
         // "this" Frame fires WindowEvent its registered WindowEvent listener 
         // "this" Frame adds "this" object as a WindowEvent listener 
  
-      setTitle("prjava03Mateo");  // "this" Frame sets title 
-      setSize(250, 100);        // "this" Frame sets initial size 
+      setTitle("Nova finestra prjava03Mateo");  // "this" Frame sets title 
+      setSize(450, 100);        // "this" Frame sets initial size 
       setVisible(true);         // "this" Frame shows 
    } 
  
