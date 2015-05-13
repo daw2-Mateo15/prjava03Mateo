9e3f6ca1457c9660f1af19c01a1fd81ca86c15e2
ubu2 <ubu2@m-ubu2>
2015-05-13 19:32:14 +0200

    Versio 0.6-C del projecte prjava03Mateo. Branca00 modificada.

--- src/prjava03mateo/Prjava03Mateo.java
+++ src/prjava03mateo/Prjava03Mateo.java
@@ -2,6 +2,7 @@ package prjava03mateo;
 
 import java.awt.*;        // using AWT containers and components 
 import java.awt.event.*;  // using AWT events and listener interfaces 
+import java.text.*;
 import java.io.*;
 
 public class Prjava03Mateo extends Frame 
